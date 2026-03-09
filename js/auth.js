7aec3817bd8457d3f47631c30e4a9dff1fd91250
KSST-TEST <kssttest@gmail.com>
2026-03-09 12:56:14 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,10 @@
-
+/* PASSWORD CHECK */
+function checkPassword() {
+    if (document.getElementById("pass").value === "Vaikuntha") {
+        document.getElementById("password-screen").style.display = "none";
+        document.getElementById("tool-screen").style.display = "block";
+        loadVSN();
+    } else {
+        alert("Incorrect Password");
+    }
+}
