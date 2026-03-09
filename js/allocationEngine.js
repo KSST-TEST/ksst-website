07d32df1507be380f8c82fd6827f0ee219061de0
KSST-TEST <kssttest@gmail.com>
2026-03-09 18:48:03 -0500

    Update allocationEngine.js

--- js/allocationEngine.js
+++ js/allocationEngine.js
@@ -1,1 +1,7 @@
+function loadStothramUI(config) {
+    // TODO: build UI (title, buttons, textareas, output area) using config
+}
 
+function runAllocation(config, options) {
+    // TODO: core allocation logic will move here from vsn.js
+}
