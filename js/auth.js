205f45e30ac88b3cc27183d5cdb30bff8cef690c
KSST-TEST <kssttest@gmail.com>
2026-03-10 13:39:41 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -5,18 +5,9 @@
 function checkPassword() {
     const entered = document.getElementById("pass").value.trim();
 
-    // Simple password check (can be upgraded later)
     if (entered === "Vaikuntha") {
-
-        // Hide login screen
         document.getElementById("password-screen").style.display = "none";
-
-        // Show tool screen
         document.getElementById("tool-screen").style.display = "block";
-
-        // Load default stothram (VSN)
-        loadVSN();
-
     } else {
         alert("Incorrect Password");
     }
